973d561c2906510e43cfdf1ba7ca716869f23878
Chris <theactionslacks@gmail.com>
2016-07-03 19:35:28 +0100

    persistence configured successfully

---
+++ grails-app/domain/grailsshopping/MajorList.groovy
@@ -0,0 +1,14 @@
+package grailsshopping
+
+class MajorList {
+
+    String listName
+    Date createdDate
+
+    static mapping = {
+        schema: "majorshopping"
+    }
+
+//    static constraints = {
+//    }
+}
